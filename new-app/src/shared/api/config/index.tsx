87d2391d637e1247d5b646597584d30121ad9caf
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-02-27 23:28:49 +0300

    fiz api routes

--- new-app/src/shared/api/config/index.tsx
+++ new-app/src/shared/api/config/index.tsx
@@ -1,2 +1,2 @@
-export const externalApiUrl = "/api" //"http://localhost:8000/api_tubes/";
+export const externalApiUrl = "/api_tubes" //"http://localhost:8000/api_tubes/";
 export const proxyApiUrl = "/api/proxy";
