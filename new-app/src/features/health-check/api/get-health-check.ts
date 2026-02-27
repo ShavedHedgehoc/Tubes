7fc56f83d8e246e57248be68bf2b9799a1a67381
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-02-27 23:59:23 +0300

    client debug

--- new-app/src/features/health-check/api/get-health-check.ts
+++ new-app/src/features/health-check/api/get-health-check.ts
@@ -4,37 +4,40 @@ type HealthCheck = {
   status: string;
 };
 
-// export async function getHealthCheck(options?: {
-//   isServer: boolean;
-// }): Promise<HealthCheck> {
-//   const client = options?.isServer ? apiClient : proxyApiClient;
-
-//   const response = await client.get<HealthCheck>("health-check", {
-//     cache: "no-store",
-//   });
-
-//   return response || { status: "ok" };
-// }
-
-
 export async function getHealthCheck(options?: {
   isServer: boolean;
 }): Promise<HealthCheck> {
-  // Если мы на сервере И в процессе сборки — возвращаем "ok" без реального запроса
   if (options?.isServer && process.env.NEXT_PHASE === 'phase-production-build') {
     return { status: "ok" };
   }
-
   const client = options?.isServer ? apiClient : proxyApiClient;
 
-  try {
-    const response = await client.get<HealthCheck>("health-check", {
-      cache: "no-store",
-    });
-    return response || { status: "ok" };
-  } catch (error) {
-    // Важно поймать ошибку, чтобы билд не упал
-    console.error("Health check failed during build/render", error);
-    return { status: "error" };
-  }
+  const response = await client.get<HealthCheck>("health-check", {
+    cache: "no-store",
+  });
+
+  return response || { status: "ok" };
 }
+
+
+// export async function getHealthCheck(options?: {
+//   isServer: boolean;
+// }): Promise<HealthCheck> {
+//   // Если мы на сервере И в процессе сборки — возвращаем "ok" без реального запроса
+//   if (options?.isServer && process.env.NEXT_PHASE === 'phase-production-build') {
+//     return { status: "ok" };
+//   }
+
+//   const client = options?.isServer ? apiClient : proxyApiClient;
+
+//   try {
+//     const response = await client.get<HealthCheck>("health-check", {
+//       cache: "no-store",
+//     });
+//     return response || { status: "ok" };
+//   } catch (error) {
+//     // Важно поймать ошибку, чтобы билд не упал
+//     console.error("Health check failed during build/render", error);
+//     return { status: "error" };
+//   }
+// }
