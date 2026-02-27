02265ceda3323f17f6f70b4581265e642af001ac
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-02-27 23:43:05 +0300

    фзш кщгеуы

--- new-app/src/shared/api/config/index.tsx
+++ new-app/src/shared/api/config/index.tsx
@@ -1,2 +1,11 @@
-export const externalApiUrl = "/api_tubes" //"http://localhost:8000/api_tubes/";
+// export const externalApiUrl = "/api_tubes" //"http://localhost:8000/api_tubes/";
+
+
+
+const IS_SERVER = typeof window === 'undefined';
+
+export const externalApiUrl = IS_SERVER
+    ? "http://api_tubes:7100/api_tubes"
+    : "/api_tubes";
+
 export const proxyApiUrl = "/api/proxy";
