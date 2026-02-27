8ab94d7a4047ab2df4d7654494123f8c4825dc84
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-02-27 22:14:04 +0300

    fix nginx

--- new-app/src/shared/api/config/index.tsx
+++ new-app/src/shared/api/config/index.tsx
@@ -1,2 +1,2 @@
-export const externalApiUrl = "http://localhost:8000/api_tubes/";
+export const externalApiUrl = "/api" //"http://localhost:8000/api_tubes/";
 export const proxyApiUrl = "/api/proxy";
