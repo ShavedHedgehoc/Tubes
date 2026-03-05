976e3dc134eb27813798f2d2089d8b5fd9a31ad8
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-06 01:21:26 +0300

    fix build

--- tubes_application/src/modules/extrusion/entry/use-extrusion-entries-handle-cards-click.ts
+++ tubes_application/src/modules/extrusion/entry/use-extrusion-entries-handle-cards-click.ts
@@ -3,6 +3,7 @@ import { useExtrusionBooleanEntryModalStore } from "../store/use-extrusion-boole
 import { useExtrusionNumericEntryModalStore } from "../store/use-extrusion-numeric-entry-modal-store";
 // import { useExtrusionRadioEntryModalStore } from "../store/use-extrusion-radio-entry-modal-store";
 import { useExtrusionIntegerEntryModalStore } from "../store/use-extrusion-integer-entry-modal-store";
+import type { ExtrusionInputParams } from "../store/use-extrusion-input-store";
 
 export default function useExtrusionEntriesHandleCardsClick() {
   const setKey = useExtrusionNumericEntryModalStore(useShallow((state) => state.setKey));
@@ -23,10 +24,6 @@ export default function useExtrusionEntriesHandleCardsClick() {
   const setBooleanTitle = useExtrusionBooleanEntryModalStore(useShallow((state) => state.setTitle));
   const setBooleanOpen = useExtrusionBooleanEntryModalStore(useShallow((state) => state.setOpen));
 
-  // const setRadioKey = useExtrusionRadioEntryModalStore(useShallow((state) => state.setKey));
-  // const setRadioTitle = useExtrusionRadioEntryModalStore(useShallow((state) => state.setTitle));
-  // const setRadioExpectedValue = useExtrusionRadioEntryModalStore(useShallow((state) => state.setExpectedValue));
-  // const setRadioOpen = useExtrusionRadioEntryModalStore(useShallow((state) => state.setOpen));
 
   const handleCardClick = ({
     id,
@@ -44,7 +41,7 @@ export default function useExtrusionEntriesHandleCardsClick() {
     setMinValue(minValue);
     setMaxValue(maxValue);
     setUnit(unit);
-    setKey(id);
+    setKey(id as ExtrusionInputParams);
     setTitle(title);
     setOpen(true);
   };
@@ -71,24 +68,11 @@ export default function useExtrusionEntriesHandleCardsClick() {
   };
 
   const handleBooleanCardClick = ({ id, title }: { id: string; title: string }) => {
-    setBooleanKey(id);
+    setBooleanKey(id as ExtrusionInputParams);
     setBooleanTitle(title);
     setBooleanOpen(true);
   };
 
-  // const handleRadioCardClick = ({
-  //   id,
-  //   title,
-  //   expectedValue,
-  // }: {
-  //   id: string;
-  //   title: string;
-  //   expectedValue: string | null | undefined;
-  // }) => {
-  //   setRadioKey(id);
-  //   setRadioTitle(title);
-  //   setRadioExpectedValue(expectedValue);
-  //   setRadioOpen(true);
-  // };
+
   return { handleCardClick, handleBooleanCardClick, handleIntegerCardClick };
 }
