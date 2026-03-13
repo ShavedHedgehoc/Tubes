cdc908088730ea40e8288e5e50858d697ef89161
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-13 20:27:56 +0300

    fix: fix status creating

--- api_tubes/src/statuses/statuses.service.ts
+++ api_tubes/src/statuses/statuses.service.ts
@@ -10,6 +10,7 @@ export class StatusesService {
   async createExtrusionStatus(dto: CreateStatusDto) {
     return await this.prisma.$transaction(async (tx) => {
       const lastStatusEntry = await tx.extrusionStatus.findFirst({
+        where: { summary_id: dto.summary_id },
         orderBy: {
           id: "desc",
         },
@@ -66,6 +67,7 @@ export class StatusesService {
   async createVarnishStatus(dto: CreateStatusDto) {
     return await this.prisma.$transaction(async (tx) => {
       const lastStatusEntry = await tx.varnishStatus.findFirst({
+        where: { summary_id: dto.summary_id },
         orderBy: {
           id: "desc",
         },
@@ -123,6 +125,7 @@ export class StatusesService {
   async createOffsetStatus(dto: CreateStatusDto) {
     return await this.prisma.$transaction(async (tx) => {
       const lastStatusEntry = await tx.offsetStatus.findFirst({
+        where: { summary_id: dto.summary_id },
         orderBy: {
           id: "desc",
         },
@@ -181,6 +184,7 @@ export class StatusesService {
   async createSealantStatus(dto: CreateStatusDto) {
     return await this.prisma.$transaction(async (tx) => {
       const lastStatusEntry = await tx.sealantStatus.findFirst({
+        where: { summary_id: dto.summary_id },
         orderBy: {
           id: "desc",
         },
@@ -234,197 +238,4 @@ export class StatusesService {
       });
     });
   }
-
-  // async createExtrusionStatus(dto: CreateStatusDto) {
-  //   const lastStatusEntry = await this.prisma.extrusionStatus.findFirst({
-  //     orderBy: {
-  //       id: "desc",
-  //     },
-  //   });
-
-  //   if (!lastStatusEntry)
-  //     throw new HttpException(
-  //       ApiMessages.PREVISIOUS_STATUS_NOT_FOUND,
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-
-  //   if (lastStatusEntry.idle) {
-  //     const timeDelta = await (new Date().getTime() -
-  //       new Date(lastStatusEntry.createdAt).getTime());
-  //     await this.prisma.extrusionStatus.update({
-  //       where: {
-  //         id: lastStatusEntry.id,
-  //       },
-  //       data: {
-  //         idle_time: timeDelta,
-  //       },
-  //     });
-  //   }
-
-  //   if (dto.defect_value) {
-  //     await this.prisma.extrusionDefect.upsert({
-  //       where: { summary_id: dto.summary_id },
-  //       update: { value: Number(dto.defect_value) },
-  //       create: { summary_id: dto.summary_id, value: Number(dto.defect_value) },
-  //     });
-  //   }
-
-  //   const extrusionEntry = await this.prisma.extrusionStatus.create({
-  //     data: {
-  //       summary_id: dto.summary_id,
-  //       operation_id: dto.operation_id,
-  //       idle: dto.idle,
-  //       finished: dto.finished,
-  //       employee_id: dto.employee_id,
-  //       counter_value: lastStatusEntry.counter_value,
-  //     },
-  //   });
-
-  //   return extrusionEntry;
-  // }
-
-  // async createVarnishStatus(dto: CreateStatusDto) {
-  //   const lastStatusEntry = await this.prisma.varnishStatus.findFirst({
-  //     orderBy: {
-  //       id: "desc",
-  //     },
-  //   });
-
-  //   if (!lastStatusEntry)
-  //     throw new HttpException(
-  //       ApiMessages.PREVISIOUS_STATUS_NOT_FOUND,
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-
-  //   if (lastStatusEntry.idle) {
-  //     const timeDelta = await (new Date().getTime() -
-  //       new Date(lastStatusEntry.createdAt).getTime());
-
-  //     await this.prisma.varnishStatus.update({
-  //       where: {
-  //         id: lastStatusEntry.id,
-  //       },
-  //       data: {
-  //         idle_time: timeDelta,
-  //       },
-  //     });
-  //   }
-
-  //   if (dto.defect_value) {
-  //     await this.prisma.varnishDefect.upsert({
-  //       where: { summary_id: dto.summary_id },
-  //       update: { value: Number(dto.defect_value) },
-  //       create: { summary_id: dto.summary_id, value: Number(dto.defect_value) },
-  //     });
-  //   }
-
-  //   const varnishEntry = await this.prisma.varnishStatus.create({
-  //     data: {
-  //       summary_id: dto.summary_id,
-  //       operation_id: dto.operation_id,
-  //       idle: dto.idle,
-  //       finished: dto.finished,
-  //       employee_id: dto.employee_id,
-  //       counter_value: lastStatusEntry.counter_value,
-  //     },
-  //   });
-
-  //   return varnishEntry;
-  // }
-
-  // async createOffsetStatus(dto: CreateStatusDto) {
-  //   const lastStatusEntry = await this.prisma.offsetStatus.findFirst({
-  //     orderBy: {
-  //       id: "desc",
-  //     },
-  //   });
-
-  //   if (!lastStatusEntry)
-  //     throw new HttpException(
-  //       ApiMessages.PREVISIOUS_STATUS_NOT_FOUND,
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-
-  //   if (lastStatusEntry.idle) {
-  //     const timeDelta = await (new Date().getTime() -
-  //       new Date(lastStatusEntry.createdAt).getTime());
-  //     await this.prisma.offsetStatus.update({
-  //       where: {
-  //         id: lastStatusEntry.id,
-  //       },
-  //       data: {
-  //         idle_time: timeDelta,
-  //       },
-  //     });
-  //   }
-
-  //   if (dto.defect_value) {
-  //     await this.prisma.offsetDefect.upsert({
-  //       where: { summary_id: dto.summary_id },
-  //       update: { value: Number(dto.defect_value) },
-  //       create: { summary_id: dto.summary_id, value: Number(dto.defect_value) },
-  //     });
-  //   }
-
-  //   const offsetEntry = await this.prisma.offsetStatus.create({
-  //     data: {
-  //       summary_id: dto.summary_id,
-  //       operation_id: dto.operation_id,
-  //       idle: dto.idle,
-  //       finished: dto.finished,
-  //       employee_id: dto.employee_id,
-  //       counter_value: lastStatusEntry.counter_value,
-  //     },
-  //   });
-
-  //   return offsetEntry;
-  // }
-
-  // async createSealantStatus(dto: CreateStatusDto) {
-  //   const lastStatusEntry = await this.prisma.sealantStatus.findFirst({
-  //     orderBy: {
-  //       id: "desc",
-  //     },
-  //   });
-
-  //   if (!lastStatusEntry)
-  //     throw new HttpException(
-  //       ApiMessages.PREVISIOUS_STATUS_NOT_FOUND,
-  //       HttpStatus.BAD_REQUEST,
-  //     );
-
-  //   if (lastStatusEntry.idle) {
-  //     const timeDelta = await (new Date().getTime() -
-  //       new Date(lastStatusEntry.createdAt).getTime());
-  //     await this.prisma.sealantStatus.update({
-  //       where: {
-  //         id: lastStatusEntry.id,
-  //       },
-  //       data: {
-  //         idle_time: timeDelta,
-  //       },
-  //     });
-  //   }
-
-  //   if (dto.defect_value) {
-  //     await this.prisma.sealantDefect.upsert({
-  //       where: { summary_id: dto.summary_id },
-  //       update: { value: Number(dto.defect_value) },
-  //       create: { summary_id: dto.summary_id, value: Number(dto.defect_value) },
-  //     });
-  //   }
-
-  //   const sealantEntry = await this.prisma.sealantStatus.create({
-  //     data: {
-  //       summary_id: dto.summary_id,
-  //       operation_id: dto.operation_id,
-  //       idle: dto.idle,
-  //       finished: dto.finished,
-  //       employee_id: dto.employee_id,
-  //       counter_value: lastStatusEntry.counter_value,
-  //     },
-  //   });
-
-  //   return sealantEntry;
-  // }
 }
