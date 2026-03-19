a77cdd7c97d99c6b19c39059f486611cb9b0f248
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-19 15:37:01 +0300

    try to fix

--- new-app/src/shared/ui/image-card/image-card.tsx
+++ new-app/src/shared/ui/image-card/image-card.tsx
@@ -5,7 +5,7 @@ export function ImageCard({ url }: { url: string }) {
         <div className="flex items-center justify-center p-2 border rounded-md bg-muted/50 w-16 h-16 overflow-hidden">
             <Image
                 src={url}
-                alt="Product Image"
+                alt="Product Image22"
                 width={64}
                 height={64}
                 unoptimized
