c9728d86e3ed73f05a66058a7289307d37611085
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-19 18:15:52 +0300

    add description field

--- new-app/src/entities/file/model/schema.ts
+++ new-app/src/entities/file/model/schema.ts
@@ -4,14 +4,18 @@ const MAX_UPLOAD_SIZE = 1024 * 1024 * 3;
 export const ACCEPTED_FILE_TYPES = ["image/jpeg", "image/png", "image/webp"];
 
 export const uploadFileFormSchema = z.object({
-  file: z
-    .instanceof(File, { message: "Необходимо выбрать файл" })
-    .refine((file) => {
-      return file.size <= MAX_UPLOAD_SIZE;
-    }, `Max file size is 3MB.`)
-    .refine((file) => {
-      return ACCEPTED_FILE_TYPES.includes(file.type);
-    }, "Only .png and .jpeg formats are accepted."),
+    file: z
+        .instanceof(File, { message: "Необходимо выбрать файл" })
+        .refine((file) => {
+            return file.size <= MAX_UPLOAD_SIZE;
+        }, `Max file size is 3MB.`)
+        .refine((file) => {
+            return ACCEPTED_FILE_TYPES.includes(file.type);
+        }, "Только .png and .jpeg"),
+    description: z
+        .string()
+        .max(300, "Максимум 300 символов"),
+
 });
 
 export type UploadFileFormValues = z.infer<typeof uploadFileFormSchema>;
