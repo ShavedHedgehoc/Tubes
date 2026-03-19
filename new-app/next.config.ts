9fc15515c29d69043608f4d34daa693accb43724
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-19 18:47:11 +0300

    fix dev docker-compose

--- new-app/next.config.ts
+++ new-app/next.config.ts
@@ -2,13 +2,26 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "standalone",
   turbopack: {
-    // Ensure this is an absolute path to your repository root
     root: path.join(__dirname, ".."),
   },
+
+  async rewrites() {
+    if (process.env.NODE_ENV !== "production") {
+      return [
+        {
+          // Когда вызываете /uploads/image.jpg на фронте
+          source: "/uploads/:path*",
+          // Проксируем на ваш API контейнер (или http://172.17.63.110*)
+          destination: "http://api_tubes:7100/uploads/:path*",
+        },
+      ];
+    }
+    return [];
+  },
+
 };
 
 export default nextConfig;
