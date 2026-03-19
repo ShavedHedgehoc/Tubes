84d7740f4fdc826ebf3d613b18401ba79196e86b
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-19 17:47:05 +0300

    add default value

--- api_tubes/prisma/migrations/20260319143918_add_file_description/migration.sql
+++ api_tubes/prisma/migrations/20260319143918_add_file_description/migration.sql
@@ -5,4 +5,4 @@
 
 */
 -- AlterTable
-ALTER TABLE "file_paths" ADD COLUMN     "description" VARCHAR(300) NOT NULL;
+ALTER TABLE "file_paths" ADD COLUMN     "description" VARCHAR(300) NOT NULL DEFAULT '';
