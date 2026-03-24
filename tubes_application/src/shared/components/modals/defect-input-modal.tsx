f473265ee485e9cbff072bd7724379b5d283ebc9
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-03-24 18:01:19 +0300

    fix modals

--- tubes_application/src/shared/components/modals/defect-input-modal.tsx
+++ tubes_application/src/shared/components/modals/defect-input-modal.tsx
@@ -37,36 +37,11 @@ export interface DefectInputModalProps {
   roundData: () => void;
 }
 
-// interface AddEntryModalButtonProps {
-//   value: string;
-//   onClick: (val: string) => void;
-// }
-
-// function AddEntryModalButton({ props }: { props: AddEntryModalButtonProps }) {
-//   return (
-//     <Button
-//       size="2xl"
-//       width="full"
-//       variant="outline"
-//       rounded="md"
-//       color="fg.subtle"
-//       onClick={() => props.onClick(props.value)}
-//     >
-//       {props.value}
-//     </Button>
-//   );
-// }
-
 export default function DefectInputModal(props: DefectInputModalProps) {
   const handleOpenchange = (e: DialogOpenChangeDetails) => {
     props.setOpen(e.open);
     props.roundData();
   };
-
-  // const handleClose = () => {
-  //   props.setOpen(false);
-  //   props.roundData();
-  // };
 
   const handleAction = (btn: (typeof BUTTONS)[0]) => {
     if (btn.action === "clear") return props.clearData();
@@ -85,7 +60,6 @@ export default function DefectInputModal(props: DefectInputModalProps) {
       placement="center"
       size="sm"
     >
-      {/* <Portal> */}
       <Dialog.Backdrop />
       <Dialog.Positioner>
         <Dialog.Content rounded="lg">
@@ -117,7 +91,6 @@ export default function DefectInputModal(props: DefectInputModalProps) {
             <Grid
               maxH="100%"
               w="100%"
-              templateRows="repeat(15, 1fr)"
               templateColumns="repeat(12, 1fr)"
               gap={2}
             >
@@ -133,53 +106,10 @@ export default function DefectInputModal(props: DefectInputModalProps) {
                   </Button>
                 </GridItem>
               ))}
-              {/* <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "C", onClick: () => props.clearData() }} />
-              </GridItem>
-              <GridItem colSpan={8} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "<<", onClick: () => props.sliceData() }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "7", onClick: () => props.changeData("7") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "8", onClick: () => props.changeData("8") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "9", onClick: () => props.changeData("9") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "4", onClick: () => props.changeData("4") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "5", onClick: () => props.changeData("5") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "6", onClick: () => props.changeData("6") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "1", onClick: () => props.changeData("1") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "2", onClick: () => props.changeData("2") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "3", onClick: () => props.changeData("3") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: ".", onClick: () => props.changeData(".") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "0", onClick: () => props.changeData("0") }} />
-              </GridItem>
-              <GridItem colSpan={4} rowSpan={3}>
-                <AddEntryModalButton props={{ value: "OK", onClick: () => handleClose() }} />
-              </GridItem> */}
             </Grid>
           </Dialog.Body>
         </Dialog.Content>
       </Dialog.Positioner>
-      {/* </Portal> */}
     </Dialog.Root>
   );
 }
