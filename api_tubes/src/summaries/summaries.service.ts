a54a6235d82f5db77780f9b234165ea10ec07964
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2026-04-17 14:35:42 +0300

    fix: fix consumed materials

--- api_tubes/src/summaries/summaries.service.ts
+++ api_tubes/src/summaries/summaries.service.ts
@@ -60,7 +60,7 @@ const summaryInclude = {
         include: {
           consumed_materials: {
             orderBy: { id: "desc" as const },
-            take: 1,
+            // take: 1,
           },
         },
       },
@@ -79,7 +79,7 @@ export interface IMappedMaterial {
 
 @Injectable()
 export class SummariesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   private async createSpecifications(
     { summaryId, value }: { summaryId: number; value: string },
@@ -218,8 +218,8 @@ export class SummariesService {
       }),
       product: query.code
         ? {
-            code: { contains: query.code, mode: "insensitive" },
-          }
+          code: { contains: query.code, mode: "insensitive" },
+        }
         : undefined,
       ...(query.states?.includes(2) && { isFinished: true }),
       ...(query.states?.includes(1) && { isActive: true }),
@@ -281,13 +281,15 @@ export class SummariesService {
   ): IMappedMaterial[] {
     return specs
       .filter((s) => s.material.post_number === postNumber)
-      .map((s) => {
-        const hasBeenScannedInThisSummary = s.material.consumed_materials.some(
-          (cm) => cm.summary_id === currentSummaryId,
+      .map(({ material }) => {
+        const hasBeenScannedInThisSummary = material.consumed_materials.some(
+          (cm) =>
+            cm.summary_id === currentSummaryId &&
+            cm.material_id === material.id
         );
         return {
-          code: s.material.code,
-          name: s.material.name,
+          code: material.code,
+          name: material.name,
           // scanned: s.material.consumed_materials.length > 0,
           scanned: hasBeenScannedInThisSummary,
         };
@@ -359,31 +361,31 @@ export class SummariesService {
 
       const current: IStatus | null = last
         ? {
-            idle: last.idle ?? false,
-            finished: last.finished ?? false,
-            state:
-              last.finished === true
-                ? "finished"
-                : last.idle === true
-                  ? "idle"
-                  : "working",
-            operation_description:
-              last.operation?.description ??
-              last.maintenance_session?.maintenance?.description ??
-              "Нет описания",
-            createdAt: last.createdAt,
-            operation_id: last.operation_id,
-            maintenance_session_id: last.maintenance_session_id,
-          }
+          idle: last.idle ?? false,
+          finished: last.finished ?? false,
+          state:
+            last.finished === true
+              ? "finished"
+              : last.idle === true
+                ? "idle"
+                : "working",
+          operation_description:
+            last.operation?.description ??
+            last.maintenance_session?.maintenance?.description ??
+            "Нет описания",
+          createdAt: last.createdAt,
+          operation_id: last.operation_id,
+          maintenance_session_id: last.maintenance_session_id,
+        }
         : {
-            idle: false,
-            finished: false,
-            state: "working",
-            operation_description: "-",
-            createdAt: null,
-            operation_id: null,
-            maintenance_session_id: null,
-          };
+          idle: false,
+          finished: false,
+          state: "working",
+          operation_description: "-",
+          createdAt: null,
+          operation_id: null,
+          maintenance_session_id: null,
+        };
 
       const counters: IStatusCounter[] = postStatuses.map((s) => ({
         counter_value: Number(s.counter_value) || 0,
